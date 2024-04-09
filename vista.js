e566c77a653d02fbddf09f6d28627546291273a1
pvolo <riv.salamanca@duocuc.cl>
2024-04-08 21:43:14 -0400

    odaa

---
+++ vista.js
@@ -0,0 +1,44 @@
+const inputQuantity =document.querySelector('input-quantity')
+const btnIncrement = document.querySelector('#incremennt')
+const btnDecrement = document.querySelector('#decrement')
+
+let valueByDefault = parseInt(inputQuantity.value)
+
+
+//funciones
+
+btnIncrement.addEventListener('click', () => {
+    valueByDefault+=1
+    inputQuantity.value=valueByDefault
+})
+
+btnDecrement.addEventListener('click', () => {
+    if(valueByDefault===1){
+    return
+}
+    valueByDefault-=1
+    inputQuantity.value=valueByDefault
+})
+
+//Toggle
+
+const ToggleDescription=document.querySelector('.title-description')
+const ToggleAdditionalInformation=document.querySelector('title-additional-information')
+
+const contentDescription=document.querySelector('.text-description')
+const contentAdditionalInformation=document.querySelector('text-additional-information')
+
+ToggleDescription.addEventListener('click', () => {
+    contentDescription.classList.toggle('hidden');
+    
+    
+    })
+
+ToggleAdditionalInformation.addEventListener('click', () => {
+    contentAdditionalInformation.classList.toggle('hidden');
+        
+        
+        })
+
+
+
